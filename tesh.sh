0dba7de2823fe351910caa26fa579afc1f085f97
Toan Tran <toantdfu@gmail.com>
2017-12-14 18:20:42 +0700

    abc

--- tesh.sh
+++ tesh.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 STATUS=$(git status)
-if [! echo "${STATUS}" | grep -q "up-to-date.*clean"]
+if ! echo ${STATUS} | grep -q "up-to-date.*clean"
 then
   echo "Branch is not up-to-date or clean, aborting"
   exit 1
